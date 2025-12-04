2360ba73bb1c96c18feba01caa031f20df3ffa9c
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-04 11:46:55 -0800

    cross-compile tweaks

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,13 +4,14 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
+export PKG_CONFIG_PATH="${BUILD_PREFIX}/lib/pkg-config:${PREFIX}/lib/pkg-config"
 
 cargo-bundle-licenses \
     --format yaml \
     --output THIRDPARTY.yml
 
 # build statically linked binary with Rust
-export LIBCLANG_PATH="${PREFIX}/lib"
+export LIBCLANG_PATH="${BUILD_PREFIX}/lib"  # only used for a build binary
 if [[ ${target_platform} =~ .*osx.* ]]; then
     export CFLAGS="${CFLAGS} -fno-define-target-os-macros"
 fi
